39a71e43ef87aa576451d21fe23e41a066317176
Gabe31415 <scot.gabriel@gmail.com>
2018-08-05 16:10:31 -0500

    checking if "run as administrator"
    added logic to make sure "run as administrator" was used

--- main.ps1
+++ main.ps1
@@ -3,6 +3,19 @@
 # for your current ADMINISTRATOR powershell session run:
 # 'Set-ExecutionPolicy Bypass -Scope Process'
 
+# if PS ver4, or greater, stop running if not "run as administrator"
+#Requires -RunAsAdministrator
+
+# if PS version is LESS THAN version 4, this will make sure you are "run as administrator"
+if ($PSVersionTable.PSVersion.Major -lt 4) {
+    if (-Not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+        write-host "Exiting!!!!!"
+        write-host "BECAUSE: You are running a version of powershell less than version 4 AND you aren't running powershell as an ADMINISTRATOR"
+    }
+    pause
+    exit
+}
+
 # Import psm1 files in same folder as this script
 # used '-Force' so that changes during each psm1 modification
 #   will update in the current running powershell instance
